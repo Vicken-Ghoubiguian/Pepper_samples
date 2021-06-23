5534709768b555fc1705571978f177ddbe575909
Eric Ghoubiguian <ericghoubiguian@live.fr>
2021-06-23 10:55:30 +0200

    Year null 2

---
+++ choregrapheProjets/display_Karine_Janet/translations/translation_en_US.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<<<<<<< HEAD
+<TS version="2.1" language="en_US"/>
+=======
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Je suis un vrais mec...</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Je suis un vrais mec...</translation>
+        </message>
+    </context>
+</TS>
+>>>>>>> 642c661da4857fbae84b062864d6df3159b81e99
