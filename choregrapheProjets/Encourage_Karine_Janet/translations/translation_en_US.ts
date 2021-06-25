ba3bf96ced9a2a8208cc9dbbe959798ee0a6f9b6
Eric Ghoubiguian <ericghoubiguian@live.fr>
2021-06-25 08:55:24 +0200

    Dev

--- choregrapheProjets/Encourage_Karine_Janet/translations/translation_en_US.ts
+++ choregrapheProjets/Encourage_Karine_Janet/translations/translation_en_US.ts
@@ -1,3 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US"/>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Coucou Karine, voici un petit message que tu n'es pas prête d'oublier...</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Coucou Karine, voici un petit message que tu n'es pas prête d'oublier...</translation>
+        </message>
+    </context>
+</TS>
