37f60784c6b3423a737ba11be58544ee9ae9db51
Eric Ghoubiguian <ericghoubiguian@live.fr>
2021-07-21 10:47:28 +0200

    Adding dialog at the end of the program

--- choregrapheProjets/Display_Karine_Janet/translations/translation_en_US.ts
+++ choregrapheProjets/Display_Karine_Janet/translations/translation_en_US.ts
@@ -1,3 +1,23 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US"/>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Bye bye Karine</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Bye bye Karine</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Bye bye Karine. Tu vas nous manquer...</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Bye bye Karine. Tu vas nous manquer...</translation>
+        </message>
+    </context>
+</TS>
--- choregrapheProjets/Display_Karine_Janet/translations/translation_fr_FR.ts
+++ choregrapheProjets/Display_Karine_Janet/translations/translation_fr_FR.ts
@@ -1,3 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fr_FR"/>
+<TS version="2.1" language="fr_FR">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Bonjour</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Bye bye Karine. Tu vas nous manquer...</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+</TS>
